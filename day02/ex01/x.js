d113d30a99cacb0d91ba43ba7240488ff04120c2
Jack Watters <squawfreeskier@gmail.com>
2019-03-19 11:00:54 -0700

    d1/ex01

---
+++ day02/ex01/x.js
@@ -0,0 +1,15 @@
+// var burt = () => {
+//     for (var x = 0; x < 1000; x++)
+//     {
+//         process.stdout.write("x");
+//     }
+// }
+
+function printx() {
+    for (var x = 0; x < 1000; x++)
+    {
+        process.stdout.write("x");
+    }
+}
+
+printx();
