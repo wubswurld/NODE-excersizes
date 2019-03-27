eddbfc1ff73ffca23f8e2f30931d3ce71a4b4562
Jack Watters <squawfreeskier@gmail.com>
2019-03-27 13:04:32 -0700

    ok

--- day01/ex03/ft_split.js
+++ day01/ex03/ft_split.js
@@ -1,4 +1,20 @@
-function    ft_split(str) {
+// function    ft_split(str) {
+//     let ret = new Array();
+//     str = str.split(' ');
+//     for (var x = 0; x < str.length; x++){
+//         if (str[x]){
+//             //console.log(ret.push(str[x]));
+//             ret.push(str[x]);
+//         }
+//         console.log(x + ' => ' + ret);
+//     }
+//     return(ret)
+// }
+
+// ft_split('bow wow wow yippee yo yippee yay');
+
+function    ft_split() {
+    var str = process.argv[2]
     let ret = new Array();
     str = str.split(' ');
     for (var x = 0; x < str.length; x++){
