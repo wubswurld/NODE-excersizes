82241c0d486119ae6c48249fcdfe4b919fc53c7b
Jack Watters <squawfreeskier@gmail.com>
2019-06-11 15:46:47 -0700

    plz

---
+++ day01/ex09/ssap2.js
@@ -0,0 +1,13 @@
+function ssap() {
+    var str = process.argv[2]
+    var arr = new Array();
+    str = str.split(' ');
+    for (let x = 0; x < str.length; x++){
+        if (str[x]){
+            arr.push(str[x]);
+        }
+    }
+    console.log(arr.sort().join(' '))
+}
+
+ssap();
